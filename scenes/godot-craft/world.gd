65c95d76707c2aff90f0b5bfef31e3638561a4b6
Timothy Cope <kirbycope@gmail.com>
2024-08-22 22:28:00 -0700

    wip

---
+++ scenes/godot-craft/world.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	# Disable the mouse pointer and capture the motion
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scenes/godot-siblings/coin_gold.gd
+++ scenes/godot-siblings/coin_gold.gd
@@ -1,4 +1,4 @@
-extends StaticBody3D
+extends Node3D
 
 # The speed the coin rotates
 var rotation_speed: float = 100.0
@@ -13,3 +13,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var rotation_amount = deg_to_rad(rotation_speed) * delta
 	rotation.y += rotation_amount
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		# Emit the spark
+		$GPUParticles3D.emitting = true
+		# Spin the coin faster
+		rotation_speed = 500
+		# Move the coin
+		var tween = get_tree().create_tween()
+		tween.tween_property($Model, "position", Vector3(0.0, 0.5, 0.0), 0.8)
+		tween.tween_callback(go_away)
+		# Wait a moment (for the tween)
+		await get_tree().create_timer(0.2).timeout
+		# Play sound
+		$AudioStreamPlayer3D.play()
+
+func go_away():
+	queue_free()
