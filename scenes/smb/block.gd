21faf5b3c018b05e6a18afb66b4a1d8ddaf5f197
Timothy Cope <kirbycope@gmail.com>
2024-08-30 10:17:59 -0700

    bump

--- scenes/smb/block.gd
+++ scenes/smb/block.gd
@@ -1,11 +1,11 @@
-extends AnimatedSprite2D
+extends CharacterBody2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		# Play animation
+		$AnimationPlayer.play("bump")
 		# Play sound effect
-		var sfx = load("res://assets/sounds/smb/Coin.wav")
+		var sfx = load("res://assets/sounds/smb/Bump.wav")
 		Globals.main_sound_player.stream = sfx
 		Globals.main_sound_player.play()
-		# Remove _this_ node from the scene.
-		queue_free()
