9b43a0d361787017595e8a7efd882c7540dad30c
Timothy Cope <kirbycope@gmail.com>
2024-09-06 23:34:50 -0700

    Remote recommendations

--- scenes/main-menu/main-menu.gd
+++ scenes/main-menu/main-menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var picks = null
+var recommendations = null
 @onready var username = $Main/Username
 
 
@@ -22,38 +23,130 @@ func _ready() -> void:
 
 	# Check if the "Picks" haven't been populated
 	if picks == null:
-		
-		# WIP
-		$HTTPRequest.request("https://github.com/kirbycope/godot-minecraft/blob/main/ci%5Cgodot-minecraft.pck")
-		
+
 		# Get the "Picks" from disk (as a string)
-		var json_as_text = FileAccess.get_file_as_string("res://scenes/main-menu/picks.json")
-		
+		var json_as_string = FileAccess.get_file_as_string("res://scenes/main-menu/picks.json")
+
 		# Convert the string to a JSON object
-		picks = JSON.parse_string(json_as_text)
-		
+		picks = JSON.parse_string(json_as_string)
+
 		# Iterate over each "Pick"
 		for i in range(len(picks)):
-			
+
 			# Define the "thumbnail" path
 			var texture_path = "Main/Picks/Pick" + str(i) + "/Pick"+ str(i) + "Button/Thumbnail"
 			# Set the "thumbnail" texture
 			get_node(texture_path).texture = load(picks[i].thumb)
-			
+
 			# Define the "title" path
 			var title_path = "Main/Picks/Pick" + str(i) + "/Title"
 			# Set the "title" text
 			get_node(title_path).text = picks[i].title
-			
+
 			# Define the "description" path
-			var decription_path = "Main/Picks/Pick" + str(i) + "/Description"
+			var description_path = "Main/Picks/Pick" + str(i) + "/Description"
 			# Set the "description" text
-			get_node(decription_path).text = picks[i].short
-			
+			get_node(description_path).text = picks[i].short
+
 			# Connect the button
 			var button_path = "Main/Picks/Pick" + str(i) + "/Pick"+ str(i) + "Button"
 			var button = get_node(button_path)
 			button.pressed.connect(_on_pick_button_pressed.bind(i))
+
+	# Check if the "Recommendations" haven't been populated
+	if recommendations == null:
+
+		# Get the "Recommendations" from disk (as a string)
+		var json_as_string = FileAccess.get_file_as_string("res://scenes/main-menu/recommendations.json")
+
+		# Convert the string to a JSON object
+		recommendations = JSON.parse_string(json_as_string)
+
+		# Iterate over each "Recommendation"
+		for i in range(len(recommendations)):
+
+			# Define the "thumbnail" path
+			var thumbnail_button_path = "Main/Recommendations/Recommendation" + str(i) + "/Recommendation"+ str(i) + "Button/Thumbnail"
+			# Get the "thumbnail" url
+			var thumb_url = recommendations[i].thumb
+			# Download the "thumbnail" (and apply it)
+			download_thumbnail(recommendations[i].pack, thumbnail_button_path, thumb_url)
+
+			# Define the "title" path
+			var title_path = "Main/Recommendations/Recommendation" + str(i) + "/Title"
+			# Set the "title" text
+			get_node(title_path).text = recommendations[i].title
+
+			# Define the "description" path
+			var description_path = "Main/Recommendations/Recommendation" + str(i) + "/Description"
+			# Set the "description" text
+			get_node(description_path).text = recommendations[i].short
+
+			# Connect the button
+			var button_path = "Main/Recommendations/Recommendation" + str(i) + "/Recommendation"+ str(i) + "Button"
+			var button = get_node(button_path)
+			button.pressed.connect(_on_recommendation_button_pressed.bind(i))
+
+
+## Download resource pack (`.pck`).
+func download_pack(pack_name: String, pack_scene: String, pack_url: String):
+
+	# Create a new HTTPRequest node
+	var http_request = HTTPRequest.new()
+
+	# Add the node to the scene
+	add_child(http_request)
+
+	# Set the response "callback"
+	http_request.request_completed.connect(_on_download_pack_completed.bind(pack_name, pack_scene))
+
+	# Initiate the request
+	http_request.request(pack_url)
+
+
+func _on_download_pack_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray, pack_name: String, pack_scene: String):
+
+	# Define a path for storing the downloaded .pck file
+	var pack_path = "user://" + pack_name + ".pck"
+	
+	# Save the binary data as a .pck file
+	var file = FileAccess.open(pack_path, FileAccess.WRITE)
+	file.store_buffer(body)
+	file.close()
+
+	# Load the resource pack from the temporary file
+	ProjectSettings.load_resource_pack(pack_path)
+
+	# Load the main scene from the resource pack
+	Globals.client.load_scene(pack_scene)
+
+
+## Download and apply thumbnail.
+func download_thumbnail(pack_name: String, thumbnail_button_path: String, thumb_url: String):
+
+	# Create a new HTTPRequest node
+	var http_request = HTTPRequest.new()
+
+	# Add the node to the scene
+	add_child(http_request)
+
+	# Set the response "callback"
+	http_request.request_completed.connect(_on_download_thumbnail_completed.bind(pack_name, thumbnail_button_path))
+
+	# Initiate the request
+	http_request.request(thumb_url)
+
+
+func _on_download_thumbnail_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray, pack_name: String, thumbnail_button_path: String):
+
+	# Load the image from the response body
+	var image = Image.new()
+	image.load_png_from_buffer(body)
+	var texture: ImageTexture = ImageTexture.create_from_image(image)
+
+	# Set the "thumbnail" texture
+	var thumbnail_button = get_node(thumbnail_button_path)
+	thumbnail_button.texture = texture
 
 
 ## Handle the Godot icon button _pressed_.
@@ -70,6 +163,8 @@ func _on_pick_button_pressed(index: int):
 	Globals.client.load_scene(picks[index].scene)
 
 
-func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	pass # Replace with function body.
-	print(response_code)
+## Handle the Recommendation thumbnail _pressed_.
+func _on_recommendation_button_pressed(index: int) -> void:
+
+	# Download the resource pack (`.pck`)
+	download_pack(recommendations[index].pack, recommendations[index].scene, recommendations[index].url)
