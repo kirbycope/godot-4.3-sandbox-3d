064406e15655db617ff10bd9a6bd25afe416e333
Timothy Cope <kirbycope@gmail.com>
2024-11-11 13:39:09 -0800

    portaly

--- scenes/portal/blue_portal.gd
+++ scenes/portal/blue_portal.gd
@@ -2,39 +2,34 @@ extends Node3D
 
 var camera: Camera3D
 var mesh_instance: MeshInstance3D
+var material_override: Material
 var sub_viewport: SubViewport
+var sub_viewport_texture: ViewportTexture
 
 
 func _ready() -> void:
-	$MeshInstance3D/SubViewport/Camera3D.current = false
 
-func _process(delta: float) -> void:
+	# Get the MeshInstance3D
+	mesh_instance = $MeshInstance3D
 
-	# Check if the camera isn't already set
-	if camera == null:
+	# Create the MeshInstance3D's material override
+	mesh_instance.material_override = StandardMaterial3D.new()
+	material_override = mesh_instance.material_override
 
-		# Try to get the camera
-		camera = $"../RedPortal/MeshInstance3D/SubViewport/Camera3D"
+	# Get the Camera3D
+	camera = $"../RedPortal/MeshInstance3D/SubViewport/Camera3D"
 
-		# If there is a camera
-		if camera != null:
 
-			# Ensure the camera renders to the SubViewport
-			camera.current = true
+func _process(_delta: float) -> void:
 
-			# Get the SubViewPort
-			var sub_viewport = $MeshInstance3D/SubViewport
+	# If there is a camera
+	if camera != null:
 
-			# Get the texture from the viewport
-			var texture = sub_viewport.get_texture()
+		# Move the camera (as a child of a SubViewport, it will not move with its parent node)
+		camera.global_transform = global_transform
+		camera.global_rotation_degrees.y = -camera.global_rotation_degrees.y
+		if camera.global_rotation_degrees.y == 0:
+			camera.global_rotation_degrees.y = 180
 
-			# Get the mesh instance
-			var mesh_instance = $MeshInstance3D
-
-			# Apply the texture to the MeshInstance3D's material
-			if mesh_instance.material_override == null:
-				mesh_instance.material_override = StandardMaterial3D.new()
-
-			# Update the material for the mesh instance
-			var material = mesh_instance.material_override
-			material.albedo_texture = texture
+		# Set the texture from the viewport
+		material_override.albedo_texture = $MeshInstance3D/SubViewport.get_texture()
