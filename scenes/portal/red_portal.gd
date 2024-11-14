074b7ae3dd75939f9705664ef678bbdb5649dd29
Timothy Cope <kirbycope@gmail.com>
2024-11-13 16:04:50 -0800

    portaly

--- scenes/portal/red_portal.gd
+++ scenes/portal/red_portal.gd
@@ -1,12 +1,14 @@
 extends Node3D
 
 var camera: Camera3D
+@onready var camera_mount: Node3D = $CameraMount
 var mesh_instance: MeshInstance3D
 var material_override: Material
 var sub_viewport: SubViewport
 var sub_viewport_texture: ViewportTexture
 
 
+## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 
 	# Get the MeshInstance3D
@@ -16,20 +18,31 @@ func _ready() -> void:
 	mesh_instance.material_override = StandardMaterial3D.new()
 	material_override = mesh_instance.material_override
 
-	# Get the Camera3D
-	camera = $"../BluePortal/MeshInstance3D/SubViewport/Camera3D"
 
-
+## Called every frame. '_delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 
-	# If there is a camera
+	# Get the 🎥 Camera behind the 🔵 Blue Portal
+	camera = $"../BluePortal/MeshInstance3D/SubViewport/Camera3D"
+
+	# Check if the camera was located
 	if camera != null:
 
-		# Move the camera (as a child of a SubViewport, it will not move with its parent node)
-		camera.global_transform = global_transform
-		camera.global_rotation_degrees.y = -camera.global_rotation_degrees.y
-		if camera.global_rotation_degrees.y == 0:
-			camera.global_rotation_degrees.y = 180
+		# Move the 🎥 camera from behind the 🔵 Blue Portal to behind the 🔴 Red Portal,
+		# so that the 🔴 Red Portal's SubViewport now displays what it would look like looking out of the 🔵 Blue Portal.
+		camera.global_transform = camera_mount.global_transform
 
 		# Set the texture from the viewport
 		material_override.albedo_texture = $MeshInstance3D/SubViewport.get_texture()
+
+
+## Called when a Node3D enters _this_ Area3D.
+func _on_body_entered(body: Node3D) -> void:
+
+	# Check if collision was caused by a player
+	if body is CharacterBody3D:
+
+		# Continue only if the portal is present
+		if camera != null:
+
+			body.global_transform = $"../BluePortal/ExitPoint".global_transform
